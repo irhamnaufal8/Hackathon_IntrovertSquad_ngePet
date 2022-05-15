3a07fe00bdeab510d5a200c3b1395b018dfe2ed5
Irham Naufal <inaufal77@gmail.com>
2022-05-15 09:54:51 +0700

    Starting Project

---
+++ ngePet/ngePet/ngePetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ngePetApp.swift
+//  ngePet
+//
+//  Created by Garry on 15/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ngePetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
